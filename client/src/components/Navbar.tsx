c4a92cd58352f5028a0e86a387d59f5a86349dd7
Joshua Rolph <joshuaeris@gmail.com>
2024-09-09 09:28:15 -0600

    UI has been designed.

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const NavLink = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as='a'
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const {} = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <img
+            src='/task-app-high-resolution-logo-transparent.png'
+            alt='Task App Logo'
+            width={100}
+            height={100}
+          />
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
