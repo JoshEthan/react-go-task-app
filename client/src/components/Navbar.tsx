324c6189ef80a228abeda436321d7daec5daead5
Joshua Rolph <joshuaeris@gmail.com>
2024-09-09 11:52:11 -0600

    Ready for deployment

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,44 +3,13 @@
 import {
   Box,
   Flex,
-  Avatar,
   Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
   useColorMode,
-  Center,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-
-interface Props {
-  children: React.ReactNode;
-}
-
-const NavLink = (props: Props) => {
-  const { children } = props;
-
-  return (
-    <Box
-      as='a'
-      px={2}
-      py={1}
-      rounded={"md"}
-      _hover={{
-        textDecoration: "none",
-        bg: useColorModeValue("gray.200", "gray.700"),
-      }}
-      href={"#"}
-    >
-      {children}
-    </Box>
-  );
-};
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
